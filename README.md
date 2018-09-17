570d7b8c12d96ac673e71da779a0566fb1e33d23
Krendelev <krendelev@gmail.com>
2018-09-17 06:49:25 +0300

    implement main features

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # Image Resizer
 
-[TODO. There will be project description]
+Utility for image resizing and file format conversion. It works with `jpeg` and `png` files out of the box, for additional formats install [corresponding libraries](https://pillow.readthedocs.io/en/5.2.x/installation.html#building-from-source).
 
-# Project Goals
+## How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+## Usage
+
+To resize image provide file name and one of the three possible parameters: `--width`, `--height` or `--scale`. You can also change resulting file name and destination with `--output` parameter. You can not specify both dimensional parameters and scale factor at the same time. Missing parameter will be calculated. If both `width` and `height` are specified and new aspect ratio differs from an old one, program will warn you and proceed.
+```bash
+$ python image_resize.py test.jpg --scale 0.5 # => test__200x300.jpg
+$ python image_resize.py test.jpg --width 300 # => test__300x450.jpg
+$ python image_resize.py test.jpg --height 240 # => test__160x240.jpg
+$ python image_resize.py test.jpg --width 200 --height 200 # => test__200x200.jpg
+New aspect ratio is different from the original one. Final image will be distorted.
+```
+To convert an image change the file extension to the file format you want to convert your image to.
+```bash
+$ python image_resize.py test.jpg -o test.png # => test__200x300.png
+```
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,58 @@
+import argparse
+from PIL import Image
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def load_image(path_to_original):
+    return Image.open(path_to_original)
+
+
+def save_image(image, path_to_result):
+    name, ext = path_to_result.split('.')
+    path = '{0}__{i[0]}x{i[1]}.{1}'.format(name, ext, i=image.size)
+    image.save(path)
+
+
+def resize_image(image, width, height, scale):
+    if scale:
+        width, height = (int(size * scale) for size in image.size)
+    elif not height:
+        height = int(image.size[1] * width / image.size[0])
+    elif not width:
+        width = int(image.size[0] * height / image.size[1])
+    return image.resize((width, height))
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    parser.add_argument('--width', type=int, nargs='?')
+    parser.add_argument('--height', type=int, nargs='?')
+    parser.add_argument('--scale', type=float, nargs='?')
+    parser.add_argument('-o', '--output', nargs='?')
+    arguments = parser.parse_args()
+    if (arguments.width or arguments.height) and arguments.scale:
+        raise parser.error('You can not specify both the dimensions \
+                            and the scale factor.')
+    return arguments
 
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    try:
+        orig_image = load_image(args.file)
+    except FileNotFoundError:
+        print('File not found')
+    if (
+        (args.width and args.height) and
+        (args.width / args.height != orig_image.size[0] / orig_image.size[1])
+    ):
+        print(
+            'New aspect ratio is different from the original one.',
+            'Final image will be distorted.'
+            )
+    if not args.width and not args.height and not args.scale:
+        args.scale = 1
+    res_image = resize_image(orig_image, args.width, args.height, args.scale)
+    if not args.output:
+        args.output = args.file
+    save_image(res_image, args.output)
